c4d078107cc838533f9eeef058ba3a944b27df01
Marton Kilian <kilian.marton@gmail.com>
2016-09-08 02:50:06 -0400

    Finished factor reduction and summation

--- factor.py
+++ factor.py
@@ -8,9 +8,11 @@ class Factor(MetaArray):
         if len(b.shape) > len(a.shape):
             a = b
             b = self
+        a = Factor(a, copy=True)
+        b = Factor(b, copy=True)
         bVars = b.variables()
         bAxis = b.axes()
-        log.debug('Multiplying factors %s and %s', a, b)
+        log.info('Multiplying factors \n %s \n AND \n %s', a, b)
         #import pdb; pdb.set_trace()
         for axis, i in reversed(bAxis.items()):
             aVars = a.variables()
@@ -26,11 +28,12 @@ class Factor(MetaArray):
         for index in np.ndindex(*b.shape):
             log.debug('Multiplying %s at index %s in \'a\' by %s', a[index], index, b[index])
             a[index] *= b[index]
+        a._info[-1]['variables'] += bVars
         return a
         
     def rollaxis(self, axis, start=0):
         new_array = np.rollaxis(self, axis, start=start)
-        info = self._info
+        info = self.infoCopy()
         info.insert(start, info.pop(axis))
         new_array._info = info
         return new_array
@@ -45,10 +48,35 @@ class Factor(MetaArray):
         return Factor(ary, info=info)
 
     def sum(self, var):
-        pass
-    
-    def reduce(self, e):
-        pass
+        i = self.axes()[var]
+        info = self.infoCopy()
+        if var not in self.variables():
+            log.error('Summing out variable %s that hasn\'t been multiplied in!', var)
+        else:
+            info[-1]['variables'] = info[-1]['variables'].replace(var, '') 
+        info.pop(i)
+        return Factor(super(Factor, self).sum(axis=(i,)), info=info)
+     
+    def reduce(self, **evidence):
+        #Only possible to reduce by one node at the moment
+        var, val = evidence.popitem()
+        info = self.infoCopy()
+        i = self.axes()[var]
+        j = list(info[i]['values']).index(val)
+        log.info('Reducing by variable %s to value %s', var, val)
+        ary = self[var: j] 
+        ary._info[-1]['variables']  = ary._info[-1]['variables'].replace(var, '')
+
+        cAxes = [ k for a,k in ary.axes().iteritems() if a not in ary.variables() ]
+        cDims = tuple( ary.shape[l] for l in cAxes )
+        log.debug('Conditioning axes:\n %s', list(cAxes))
+        log.debug('Conditioning dimensions:\n %s', cDims)
+
+        for d in np.ndindex(*cDims):
+            index = tuple( slice(axisLength) if m not in cAxes else d[cAxes.index(m)] for m, axisLength in enumerate(ary.shape)  ) 
+            log.debug('Summing at index: %s', index)
+            ary[index] /= super(Factor, ary[index]).sum()
+        return ary
 
     def variables(self):
         return self.infoCopy()[-1]['variables']
--- testing.py
+++ testing.py
@@ -64,7 +64,13 @@ if __name__ == '__main__':
     print 'ED: ', ED
     #import pdb; pdb.set_trace()
     BED = B * ED
-    print BED
-    #import pdb; pdb.set_trace()
+    print 'BED: ', BED
+    ABED = A * BED
+    print 'ABED: ', ABED
+    ABED_sum = ABED.sum('A')
+    print 'ABED multiplied', ABED_sum
+    ABED_reduce = ABED_sum.reduce(B='F')
+    print 'ABED reduced factor', ABED_reduce
+    import pdb; pdb.set_trace()
     pass
 
